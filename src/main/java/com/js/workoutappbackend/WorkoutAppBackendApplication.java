278f0ff98328fb05eaa982a2ed8b0574bae275ee
Johanna Salmi Sunnerdahl <73692594+Salmihanna@users.noreply.github.com>
2022-05-16 18:46:27 +0200

    enable cors on endpoints

--- src/main/java/com/js/workoutappbackend/WorkoutAppBackendApplication.java
+++ src/main/java/com/js/workoutappbackend/WorkoutAppBackendApplication.java
@@ -3,7 +3,10 @@ package com.js.workoutappbackend;
 import com.js.workoutappbackend.repository.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackageClasses = UserRepository.class)
@@ -13,4 +16,13 @@ public class WorkoutAppBackendApplication {
         SpringApplication.run(WorkoutAppBackendApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/v1/sign-in").allowedOrigins("http://localhost:3000");
+            }
+        };
+    }
 }
